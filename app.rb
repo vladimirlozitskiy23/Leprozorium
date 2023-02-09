f9f1f0ce15d101a7e9eef57d028bd3d97a426445
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-02-09 14:59:58 +0300

    Init database, make db

--- app.rb
+++ app.rb
@@ -2,9 +2,19 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
+end
+
+def init_db
+	@db = SQLite3::Database.new 'leprozorium.db'
+	@db.results_as_hash = true
+end
+
+before do
+	init_db
 end
 
 
