eafb7c2d84df615a0c7935c5163ed51556c9e99f
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-02-10 10:37:58 +0300

    Add detail view

--- app.rb
+++ app.rb
@@ -47,5 +47,7 @@ end
 
 get '/details/:post_id' do
 	post_id = params[:post_id]
-	erb "Displaing information #{post_id}"
+	results = @db.execute 'select * from Posts where id = ?',[post_id]
+	@row = results[0]
+	erb :details
 end
